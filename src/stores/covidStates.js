12f5c1b0dd007a080d3ef38a1c2bf4f7e0203857
Edgar Tolete <57813441+edgartolete@users.noreply.github.com>
2022-12-12 02:04:02 +0800

    Sucess 0.1.0

--- src/stores/covidStates.js
+++ src/stores/covidStates.js
@@ -7,28 +7,53 @@ export const useCovidApi = defineStore('covidApi', {
         global: [],
         countries: [],
         countryNames: [],
+        findCountry: "",
         newConfirmed: "",
         newDeaths: "",
         newRecovered: "",
         totalConfirmed: "",
         totalDeaths: "",
-        totalRecovered: ""
+        totalRecovered: "",
     }),
     actions: {    
         async getData() {
-            await fetch('src/summary.json')
-                .then(res => res.json())
-                .then(data => this.fullResponse = data)
-                .then(data => this.global = data.Global)
-                .then(data => this.countries = data.Countries)
-                .then(data => this.countryNames = data.map((item)=>{
+            try{
+                let response = await fetch('src/summary.json')
+                let data = await response.json()
+                this.global = await data.Global
+                this.countries = await data.Countries;
+
+                this.newConfirmed = await this.global.NewConfirmed.toLocaleString("en-US")
+                this.newDeaths = await this.global.NewDeaths.toLocaleString("en-US")
+                this.newRecovered = await this.global.NewRecovered.toLocaleString("en-US")
+                this.totalConfirmed = await this.global.TotalConfirmed.toLocaleString("en-US")
+                this.totalDeaths = await this.global.TotalDeaths.toLocaleString("en-US")
+                this.totalRecovered = await this.global.TotalRecovered.toLocaleString("en-US")
+                
+                this.countryNames = this.countries.map((item) => {
                     return item["Country"];
-                }))
-                .catch(err => console.log(err.message))
-                .then(data => findCountryDetails("Philippines"){
-                    this.result = this.countries.filter(obj => obj.Country === country)
                 })
-            },        
+            }
+            catch(err){
+                console.log(err)
+            }
+        },
+        async findCountryDetails(country){
+            let response = await fetch('src/summary.json')
+            let data = await response.json()
+            let result = await data.Countries.filter(obj => obj.Country === country)
+            if(this.countryNames.includes(country)){
+                this.newConfirmed = await result[0].NewConfirmed.toLocaleString("en-US")
+                this.newDeaths = await result[0].NewDeaths.toLocaleString("en-US")
+                this.newRecovered = await result[0].NewRecovered.toLocaleString("en-US")
+                this.totalConfirmed = await result[0].TotalConfirmed.toLocaleString("en-US")
+                this.totalDeaths = await result[0].TotalDeaths.toLocaleString("en-US")
+                this.totalRecovered = await result[0].TotalRecovered.toLocaleString("en-US")
+            }else{
+                console.log('country not found')
+            }
+
+        }        
         
     },
     getters: {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,63 +1,83 @@
-<script setup>
-import { useCovidApi } from '../stores/covidStates'
-let covidApi = useCovidApi();
-covidApi.getData();
 
-</script>
 
 <template>
-  <h1> Covid Data</h1>
+<div class="container">
+  <h1>Covid-19 Update as of {{ dateToday }}</h1>
   <div class="stats">
     <div class="stat">
-      <p> Confirmed:</p> 
-      <span>{{ covidApi.global.NewConfirmed }}</span>
+      <p>New Confirmed:</p>
+      <span>{{ covidApi.newConfirmed }}</span>
     </div>
     <div class="stat">
       <p>New Deaths:</p>
-      <span>{{ covidApi.global.NewDeaths }}</span>
+      <span>{{ covidApi.newDeaths }}</span>
     </div>
     <div class="stat">
       <p>New Recovered:</p>
-      <span>{{ covidApi.global.NewRecovered }}</span>
+      <span>{{ covidApi.newRecovered }}</span>
     </div>
     <div class="stat">
-      <p>Total Confirmed: </p>
-      <span>{{ covidApi.global.TotalConfirmed }}</span>
+      <p>Total Confirmed:</p>
+      <span>{{ covidApi.totalConfirmed }}</span>
     </div>
     <div class="stat">
       <p>Total Deaths:</p>
-      <span>{{ covidApi.global.TotalDeaths }}</span>
+      <span>{{ covidApi.totalDeaths }}</span>
     </div>
     <div class="stat">
       <p>Total Recovered:</p>
-      <span>{{ covidApi.global.TotalRecovered }}</span>  
+      <span>{{ covidApi.totalRecovered }}</span>
     </div>
   </div>
-<p>{{ covidApi.findCountryDetails("Philippines") }}</p>
+  <input v-model="covidApi.findCountry" type="text" />
+  <button @click="covidApi.findCountryDetails(covidApi.findCountry)">Find</button>
+  <p> {{ covidApi.result }}</p>
 
-<!-- <p>Countries: {{ covidApi.global }}</p> -->
+</div>
+
+  <!-- <p>Countries: {{ covidApi.global }}</p> -->
 </template>
 
+<script setup>
+import { useCovidApi } from "../stores/covidStates";
+let covidApi = useCovidApi();
+covidApi.getData();
+
+let dateToday = new Date()
+let currentMonth = dateToday.getMonth()
+var months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+
+currentMonth = months[currentMonth];
+
+dateToday = currentMonth + " " + dateToday.getFullYear()
+
+</script>
+
+
 <style>
-.stats{
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 1rem;
+
+.container{
   max-width: 800px;
-  margin: auto;
-  text-align: right;
+  margin: auto
 }
-.stats .stat{
-  border: 1px solid black;
-  padding: 10px;
-}
+  .stats {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 1rem;
+    margin: auto;
+    text-align: right;
+  }
+  .stats .stat {
+    border: 1px solid black;
+    padding: 10px;
+  }
 
-.stats .stat p{
-  padding: 0; margin: 0;
-}
-.stats .stat span {
+  .stats .stat p {
+    padding: 0;
+    margin: 0;
+  }
+  .stats .stat span {
     font-size: 2rem;
-}
+  }
 </style>
-
-
