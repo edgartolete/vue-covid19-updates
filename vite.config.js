cbf15b384e8d4a3ddd2753978f42c3b5d3610a79
Edgar Tolete <57813441+edgartolete@users.noreply.github.com>
2022-12-15 05:55:05 +0800

    t

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./vue-covid19-updates/src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
